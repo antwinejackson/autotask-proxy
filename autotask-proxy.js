0ffde53c24dd450fdfb5a0376a5e13f9dce8319a
Antwine Jackson <ajackson@enitechsolutions.com>
2026-02-28 23:17:58 -0500

    add better error logging

--- autotask-proxy.js
+++ autotask-proxy.js
@@ -8,14 +8,16 @@ app.use(express.json());
 app.use("/autotask", async function(req, res) {
   var username = req.headers["username"];
   var apikey = req.headers["apikey"];
+  var trackingid = req.headers["trackingid"] || "";
   var zone = req.headers["zone"] || "";
   var base = "https://webservices" + zone + ".autotask.net/ATServicesRest/V1.0";
   var targetUrl = base + req.url;
   console.log("[PROXY] " + targetUrl);
   try {
-    var response = await fetch(targetUrl, { method: req.method, headers: { "UserName": username, "Secret": apikey, "ApiIntegrationCode": apikey, "Content-Type": "application/json" }, body: req.method !== "GET" ? JSON.stringify(req.body) : undefined });
-    var data = await response.json();
-    res.status(response.status).json(data);
+    var response = await fetch(targetUrl, { method: req.method, headers: { "UserName": username, "Secret": apikey, "ApiIntegrationCode": trackingid, "Content-Type": "application/json" }, body: req.method !== "GET" ? JSON.stringify(req.body) : undefined });
+    var text = await response.text();
+    console.log("[AUTOTASK RESPONSE] " + response.status + " " + text);
+    try { res.status(response.status).json(JSON.parse(text)); } catch(e) { res.status(response.status).send(text); }
   } catch (err) {
     console.error("[PROXY ERROR] " + err.message);
     res.status(500).json({ error: err.message });
